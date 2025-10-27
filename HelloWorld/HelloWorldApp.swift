2db62a43a9b855603549d2a2cec9bdc0134b8df1
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2025-10-27 17:07:57 +0100

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Armin on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
